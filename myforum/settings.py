30e2919e0b48a1b0153471bb222b04ee9c1887c7
unknown <atubu2000@gmail.com>
2024-08-01 23:32:31 +0200

    Добавлены настройки STATICFILES_FINDERS для правильной сборки статических файлов

--- myforum/settings.py
+++ myforum/settings.py
@@ -192,6 +192,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
